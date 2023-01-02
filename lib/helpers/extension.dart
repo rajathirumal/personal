1df35ab884943734bdd1918324aa821244c47c01
Raja Thirumal Govindaraj <rajathirumal98@gmail.com>
2023-01-02 16:21:55 +0530

    login page designed

---
+++ lib/helpers/extension.dart
@@ -0,0 +1,13 @@
+extension CapExtension on String {
+  String get inCaps => '${this[0].toUpperCase()}${substring(1)}';
+  String get allInCaps => toUpperCase();
+  String get capitalizeFirstofEach =>
+      split(" ").map((str) => str.capitalizeFirstofEach).join(" ");
+
+  // bool get emailRegex => RegExp(
+  //         r"^[a-zA-Z0-9.a-zA-Z0-9.!#$%&'*+-/=?^_`{|}~]+@[a-zA-Z0-9]+\.[a-zA-Z]+")
+  //     .hasMatch(this);
+  bool get emailRegex => RegExp(
+          r"^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,253}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,253}[a-zA-Z0-9])?)*$")
+      .hasMatch(this);
+}
