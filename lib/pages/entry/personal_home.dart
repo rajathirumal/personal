5962b9d877a114656dc3abf9cbcee66585a3b9be
Raja Thirumal Govindaraj <rajathirumal98@gmail.com>
2023-01-03 17:06:56 +0530

    add expense main dialog

--- lib/pages/entry/personal_home.dart
+++ lib/pages/entry/personal_home.dart
@@ -1,6 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/src/widgets/container.dart';
-import 'package:flutter/src/widgets/framework.dart';
 
 class PersonalHome extends StatefulWidget {
   const PersonalHome({super.key});
@@ -12,10 +10,53 @@ class PersonalHome extends StatefulWidget {
 class _PersonalHomeState extends State<PersonalHome> {
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
+    return Scaffold(
+      body: const Center(
         child: Text("Personal home"),
       ),
+      floatingActionButton: FloatingActionButton.extended(
+        onPressed: () => _dialogBuilder(context),
+        // () {
+        //   // Provider.of<AuthServices>(context,listen: false).signOut();
+        //   showDialog(context: context, builder: );
+
+        // },
+        label: const Text('Add expense'),
+      ),
+    );
+  }
+
+  Future<void> _dialogBuilder(BuildContext context) {
+    return showDialog<void>(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: const Text('Adding expense !?'),
+          content: const Text(
+              'If you are trying to add a fuel, please navigate to the add fuel page,\n\n'
+              'Simply click on "Take me to fuel page" '),
+          actions: <Widget>[
+            TextButton(
+              style: TextButton.styleFrom(
+                textStyle: Theme.of(context).textTheme.labelLarge,
+              ),
+              child: const Text('Take me to fuel page'),
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+            ),
+            TextButton(
+              style: TextButton.styleFrom(
+                textStyle: Theme.of(context).textTheme.labelLarge,
+              ),
+              child: const Text('Other expense'),
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+            ),
+          ],
+        );
+      },
     );
   }
 }
